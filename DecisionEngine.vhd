b005bcf5504fd68f4e2b6f200513b94371fede89
CoherenceController <qlab@bbn.com>
2014-07-01 22:53:07 -0400

    Lots of little fixes to get in synthesizing
    Since KERNEL_ADDR_WIDTH is needed by a few files move to a package
    constant rather than a generic.
    
    --CAR

--- DecisionEngine.vhd
+++ DecisionEngine.vhd
@@ -2,10 +2,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use work.ii_dsp_pkg.KERNEL_ADDR_WIDTH;
+
 entity DecisionEngine is
-  generic(
-  	KERNEL_ADDR_WIDTH : natural
-  )
   port (
 	rst : in std_logic;
 	clk : in std_logic;
@@ -33,8 +32,8 @@ architecture arch of DecisionEngine is
 
 signal addrct : unsigned(KERNEL_ADDR_WIDTH-1 downto 0) ;
 
-signal prod_re, prod_im : signed(32 downto 0) ;
-signal tmp1, tmp2, tmp3, tmp4 : signed(32 downto 0);
+signal prod_re, prod_im : signed(31 downto 0) ;
+signal tmp1, tmp2, tmp3, tmp4 : signed(31 downto 0);
 signal s_data_re, s_data_im, s_kernel_re, s_kernel_im : signed(15 downto 0);
 
 signal accum_re, accum_im : signed(31 downto 0);
@@ -86,11 +85,11 @@ begin
 			accum_re <= (others => '0');
 			accum_im <= (others => '0');
 		else
-			accum_re <= accum_re + prod_re(32 downto 17);
-			accum_im <= accum_im + prod_im(32 downto 17);
+			accum_re <= accum_re + prod_re(31 downto 16);
+			accum_im <= accum_im + prod_im(31 downto 16);
 
-			result_re <= accum_re;
-			result_im <= accum_im;
+			result_re <= std_logic_vector(accum_re);
+			result_im <= std_logic_vector(accum_im);
 		end if ;
 	end if ;
 end process ; -- accum
@@ -99,7 +98,7 @@ end process ; -- accum
 Thresholding : process( clk )
 begin
 	if rising_edge(clk) then
-		if result_re > signed(threshold) then
+		if accum_re > signed(threshold) then
 			state <= '1';
 		else
 			state <= '0';
