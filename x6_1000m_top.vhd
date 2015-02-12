32614e7800b36e3732599be20bdf144d94971334
CoherenceController <qlab@bbn.com>
2015-02-12 08:46:30 -0500

    Remove a few unused debug cores.
    They didn't seem to improve behaviour.
    
    --CAR

--- x6_1000m_top.vhd
+++ x6_1000m_top.vhd
@@ -721,7 +721,7 @@ architecture arch of x6_1000m_top is
 ------------------------------------------------------------------------------
 -- Chipscope debug
 ------------------------------------------------------------------------------
-  signal control0, control1, control2, control3 : std_logic_vector(35 downto 0) ;
+  signal control0, control1 : std_logic_vector(35 downto 0) ;
 
 -----------------------------------------------------------------------------
 signal dac0_ext_sync, dac1_ext_sync, adc0_ext_sync, adc1_ext_sync : std_logic;
@@ -2099,7 +2099,7 @@ inst_dsp0 : entity work.ii_dsp_top
 
     -- Decision Engine outputs
     state     => state0,
-    state_vld => state0_vld 
+    state_vld => state0_vld
   );
 
   inst_dsp1 : entity work.ii_dsp_top
@@ -2132,15 +2132,13 @@ inst_dsp0 : entity work.ii_dsp_top
 
     -- Decision Engine outputs
     state => state1,
-    state_vld => state1_vld 
+    state_vld => state1_vld
   );
 
   inst_chipscope_icon : entity work.chipscope_icon
   port map (
     CONTROL0 => control0,
-    CONTROL1 => control1,
-    CONTROL2 => control2,
-    CONTROL3 => control3);
+    CONTROL1 => control1);
 
   inst_chipscope_dac0 : entity work.chipscope_ila_dac
   port map (
@@ -2172,21 +2170,21 @@ inst_dsp0 : entity work.ii_dsp_top
     DATA(15 downto 0) => wf_rd_addr_copy_dac1,
     TRIG0(0) => dac1_ext_sync);
 
-  inst_chipscope_adc0 : entity work.chipscope_ila_adc
-  port map (
-    CONTROL => control2,
-    CLK => adc0_data_clk,
-    DATA(48) => adc0_ext_sync,
-    DATA(47 downto 0) => adc0_raw_data,
-    TRIG0(0) => adc0_ext_sync);
+  -- inst_chipscope_adc0 : entity work.chipscope_ila_adc
+  -- port map (
+  --   CONTROL => control2,
+  --   CLK => adc0_data_clk,
+  --   DATA(48) => adc0_ext_sync,
+  --   DATA(47 downto 0) => adc0_raw_data,
+  --   TRIG0(0) => adc0_ext_sync);
 
-  inst_chipscope_adc1 : entity work.chipscope_ila_adc
-  port map (
-    CONTROL => control3,
-    CLK => adc1_data_clk,
-    DATA(48) => adc1_ext_sync,
-    DATA(47 downto 0) => adc1_raw_data,
-    TRIG0(0) => adc1_ext_sync);
+  -- inst_chipscope_adc1 : entity work.chipscope_ila_adc
+  -- port map (
+  --   CONTROL => control3,
+  --   CLK => adc1_data_clk,
+  --   DATA(48) => adc1_ext_sync,
+  --   DATA(47 downto 0) => adc1_raw_data,
+  --   TRIG0(0) => adc1_ext_sync);
 
 ------------------------------------------------------------------------------
 -- DSP VITA mover
