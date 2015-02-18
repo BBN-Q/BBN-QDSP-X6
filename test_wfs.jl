416446f908680ad499c3e73bc6c79d58b201b797
CoherenceController <qlab@bbn.com>
2015-02-18 11:07:48 -0500

    More Julia deprecations
    --CAR

--- test_wfs.jl
+++ test_wfs.jl
@@ -1,12 +1,11 @@
 WF_LENGTH = 1024;
 
-freqs = 10;
+wf0 = vcat(round(Int16, 2^11*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
+wf1 = vcat(round(Int16, 2^11*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
 
-wf0 = [zeros(Int16, 256), int16(2^11*sin(2*pi*(20/1000)*[0:511])), zeros(Int16, 256)]
-wf1 = [zeros(Int16, 256), int16(2^11*sin(pi + 2*pi*(20/1000)*[0:511])), zeros(Int16, 256)]
-
-
-fid = open("testWFs.in", "w")
-writedlm(fid, wf0', ' ')
-writedlm(fid, wf1', ' ')
-close(fid)
+open("testWFs.in", "w") do fid
+	for ct in 1:10
+		writedlm(fid, wf0', ' ')
+		writedlm(fid, wf1', ' ')
+	end
+end
