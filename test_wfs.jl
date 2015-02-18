442fa30e7e11f285dde7c37e632f44ee50383529
CoherenceController <qlab@bbn.com>
2015-02-18 11:46:21 -0500

    Avoid ADC test data overflow
    --CAR

--- test_wfs.jl
+++ test_wfs.jl
@@ -1,7 +1,7 @@
 WF_LENGTH = 1024;
 
-wf0 = vcat(round(Int16, 2^11*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
-wf1 = vcat(round(Int16, 2^11*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
+wf0 = vcat(round(Int16, 2^11*0.9*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
+wf1 = vcat(round(Int16, 2^11*0.9*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
 
 open("testWFs.in", "w") do fid
 	for ct in 1:10
