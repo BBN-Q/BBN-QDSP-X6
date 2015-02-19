640ac5d53b12ac92bd6b1e3d6f2449e08d3df936
CoherenceController <qlab@bbn.com>
2015-02-19 15:22:34 -0500

    Put a BADDF00F at the end
    Just so we know where the end is.
    
    --CAR

--- test_wfs.jl
+++ test_wfs.jl
@@ -1,7 +1,7 @@
 WF_LENGTH = 1024;
 
-wf0 = vcat(round(Int16, 2^11*0.9*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
-wf1 = vcat(round(Int16, 2^11*0.9*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 512))
+wf0 = vcat(round(Int16, 2^11*0.9*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 508), Int16[0x00ba, 0x00ad, 0x00f0, 0x000f])
+wf1 = vcat(round(Int16, 2^11*0.9*sin(2*pi*(10/1000)*(0:511))), zeros(Int16, 508), Int16[0x00ba, 0x00ad, 0x00f0, 0x000f])
 
 open("testWFs.in", "w") do fid
 	for ct in 1:10
