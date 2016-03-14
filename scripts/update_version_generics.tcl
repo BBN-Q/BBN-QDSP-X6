7dd127dd15c87ee2ea328e29c7fafedef1992bc2
Colm Ryan <cryan@bbn.com>
2016-03-13 20:11:32 -0400

    fixes for version generics
    * actually parse git describe
    * regexp for hex in previous generics

--- scripts/update_version_generics.tcl
+++ scripts/update_version_generics.tcl
@@ -6,7 +6,18 @@ cd $REPO_ROOT
 
 #get the version info
 set git_descrip [exec git describe --dirty]
-regexp {v(\d+)\.(\d+)} [exec git describe] -> tag_major tag_minor
+regexp {v(\d+)\.(\d+)(\-\d+\-)?} $git_descrip -> tag_major tag_minor commits_since
+if {$commits_since eq ""} {
+  set commits_since 0
+} else {
+  regexp {\-(\d+)\-} $commits_since -> commits_since
+}
+if { [regexp {\-dirty} $git_descrip] == 1} {
+  set dirty d
+} else {
+  set dirty 0
+}
+
 set git_sha1 [exec git rev-parse --short=8 HEAD]
 
 #and the timestamp
@@ -16,9 +27,9 @@ set build_timestamp [clock format [clock seconds] -format {32'h%y%m%d%H}]
 #(bizarrely we have to use Verilog style for std_logic_vector generics see https://forums.xilinx.com/t5/Vivado-TCL-Community/Vivado-2013-1-and-VHDL-93-string-generics/m-p/333341/highlight/true)
 set generics [project get "Generics, Parameters"]
 #x6_version
-regsub {BBN_X6_VERSION=32'h\d{8}} $generics [format BBN_X6_VERSION=32'h%x%03x%02x%02x 13 10 0 9] generics
+regsub {BBN_X6_VERSION=32'h[0-9a-fA-F]{8}} $generics [format BBN_X6_VERSION=32'h$dirty%03x%02x%02x $commits_since $tag_major $tag_minor] generics
 #git_sha1
-regsub {BBN_X6_GIT_SHA1=32'h\d{8}} $generics BBN_X6_GIT_SHA1=32'h$git_sha1 generics
+regsub {BBN_X6_GIT_SHA1=32'h[0-9a-fA-F]{8}} $generics BBN_X6_GIT_SHA1=32'h$git_sha1 generics
 #build timestamp
 regsub {BUILD_TIMESTAMP=32'h\d{8}} $generics BUILD_TIMESTAMP=$build_timestamp generics
 
