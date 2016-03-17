9a7debd91c470cab6c45916e9c5ceae64dcfb185
Colm Ryan <cryan@bbn.com>
2016-03-17 17:09:30 -0400

    :white_check_mark: add test runner and script for KernelIntegrator

---
+++ test/scripts/KernelIntegrator_tb/xsim/filelist.f
@@ -0,0 +1,5 @@
+../../../../deps/VHDL-Components/src/DelayLine.vhd
+../../../../deps/VHDL-Components/src/ComplexMultiplier.vhd
+../../../../src/BBN_QDSP_pkg.vhd
+../../../../src/KernelIntegrator.vhd
+../../../KernelIntegrator_tb.vhd
